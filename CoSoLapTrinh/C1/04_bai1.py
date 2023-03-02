7a890c958e37792f5fdbded6a370b88c33c71013
mylinhsan <125127432+mylinhsan@users.noreply.github.com>
2023-03-02 07:24:31 +0700

    Update 04_bai1.py

--- CoSoLapTrinh/C1/04_bai1.py
+++ CoSoLapTrinh/C1/04_bai1.py
@@ -2,12 +2,16 @@
 # b=2
 # print("Chia het :",a/b)
 # print("Chia lay phan nguyen: ", a//b)
-# print("Chia lay phan du: ",a%b
+# print("Chia lay phan du: ",a%b)
+
+
 # a=5
 # b=10
 # print(a,b,a+b)
 # print("a+b=", a+b)
 # print("a+b=",a+b)
+
+
 # a=5
 # b=10
 # print(a,end=' ')
